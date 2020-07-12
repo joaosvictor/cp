decf82b9c3419dfbd5fb5f3789fda07fc42f98c7
Joao Victor <48931503+joaovictorsantos@users.noreply.github.com>
2020-07-12 14:42:01 -0300

    Create setup_geany_CP.py

---
+++ setup_geany_CP.py
@@ -0,0 +1,20 @@
+from collections import deque
+import datetime
+import math
+import os
+import random
+import re
+import sys
+#n=list(map(int, input().split()))
+#n=map(int, input().split())
+
+def main():
+#code go here
+main()
+
+
+
+'''
+if __name__ == '__main__':
+	main()
+'''
