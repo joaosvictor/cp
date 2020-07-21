57d86aa5f3f1216ece3f9079f5d0276000ea67ee
Joao Victor <48931503+joaovictorsantos@users.noreply.github.com>
2020-07-21 19:59:45 -0300

    Update setup_geany_CP.py

--- setup_geany_CP.py
+++ setup_geany_CP.py
@@ -1,20 +1,37 @@
-from collections import deque
-import datetime
-import math
-import os
-import random
 import re
+import os
 import sys
+import math
+import turtle
+import struct
+import random
+import string
+import datetime
+import calendar
+import statistics
+from time import sleep 
+from decimal import Decimal 
+from collections import deque
+from fractions import Fraction as Fra
 #n=list(map(int, input().split()))
 #n=map(int, input().split())
 
+mxN=1e5 
+mxM=100
+
+'''
+def dfs(arg):
+	return arg[-1]==#
+    graph={}
+    graph['a']=1
+'''
+
+def solve():
+	#code goes here
+		
 def main():
-#code go here
+	#t=int(input())
+	#for z in range(t):
+		solve()
+		#dfs()
 main()
-
-
-
-'''
-if __name__ == '__main__':
-	main()
-'''
