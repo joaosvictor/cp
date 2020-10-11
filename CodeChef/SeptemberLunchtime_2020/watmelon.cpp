b525690101119f356889b42f466ab32168a073cb
João Victor <joaosantoos@aol.com>
2020-10-11 18:30:34 -0300

    1º

---
+++ CodeChef/SeptemberLunchtime_2020/watmelon.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+
+using namespace std ;
+
+int main() {
+	int t ;
+	cin >> t ;
+	while(t--){
+		int N ;
+		cin >> N ;
+
+		int B[N];
+		for( int i = 0 ; i < N ; i++ ){
+			cin >> B[i] ;
+
+		}
+		int sum = 0 ;
+		for( int i = 0 ; i < N ; i++ ){
+			sum = sum + B[i];
+		}
+		if(sum < 0) {
+			cout << "NO" << endl ;		
+		}else if( sum >= 0 ){
+			cout << "YES" << endl ;
+		}
+	}
+	return 0 ;
+}
