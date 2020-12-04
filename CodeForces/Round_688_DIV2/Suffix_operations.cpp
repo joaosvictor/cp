5b3d8c015d8d6abbe948f4368fa7d466c02865ee
João Victor <joaosantoos@aol.com>
2020-12-04 16:00:17 -0300

    Create Suffix_operations.cpp

---
+++ CodeForces/Round_688_DIV2/Suffix_operations.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+long long a[200001],s[200001];
+
+int main(){
+
+	int T;
+	cin>>T;
+	while(T--){
+	
+		int n;
+		cin>>n;
+		for(int i=0;i<n;i++){
+			cin>>a[i];
+		}
+    
+		s[0]=0;
+		for(int i=1;i<n;i++)
+		s[i]=abs(a[i]-a[i-1])+s[i-1];
+		long long ans=s[n-1]-abs(a[1]-a[0]);
+    
+		for(int i=1;i<n-1;i++)
+    
+		ans = min(ans,abs(a[i+1]-a[i-1])+s[i-1]+s[n-1]-s[i+1]);
+		ans = min(ans,s[n-2]);
+		cout<<ans<<endl;	
+    
+	}
+ } 
