2f5407a22d13cfab2ba56e47f4a01180d7c96bb9
João Victor <joaosantoos@aol.com>
2020-12-19 23:38:53 -0300

    day 19

--- Advent_of_code_2020/day19/solve19.py
+++ Advent_of_code_2020/day19/solve19.py
@@ -1,32 +1,63 @@
 #!/usr/bin/env python3
-import re
-from lark import Lark
+
+def matches(rules, rule, string):
+    return any(m == '' for m in match_helper(rules, rule, string))
 
 
-with open('input.txt') as f:
-    data = f.read()
+def match_helper(rules, rule, string):
+    rule_sets = rules[rule]
+    if isinstance(rule_sets, str):
+        if string.startswith(rule_sets):
+            yield string[len(rule_sets):]
+        return
 
-rules, messages = data.split('\n\n')
-messages = messages.split()
+    for rs in rule_sets:
+        suffixes = [string]
+        new_suffixes = []
+        for rule in rs:
+            for m in suffixes:
+                new_suffixes.extend(match_helper(rules, rule, m))
+            suffixes = new_suffixes
+            new_suffixes = []
+        yield from suffixes
 
 
-def solve(rules):
-    rules = 'start: t0\n' + re.sub(r'(\d+)', r't\1', rules)
-    parser = Lark(rules)
-    valid = len(messages)
-    for message in messages:
-        try:
-            parser.parse(message)
-        except:
-            valid -= 1
-    return valid
+def parse_rules(file1):
+    rules = {}
+    for line in file1:
+        left, right = line.split(': ')
+        if right.startswith('"'):
+            rules[left] = right.strip('"')
+        else:
+            rule_sets = right.split(' | ')
+            rules[left] = [rule_set.split() for rule_set in rule_sets]
+    return rules
 
-print('Answer to part 1: ')
-print(solve(rules))
 
-print('')
-print('Answer to part 2:')
-rules = rules\
-    .replace('8: 42', '8: 42 | 42 8')\
-    .replace('11: 42 31', '11: 42 31 | 42 11 31')
-print(solve(rules))
+def part1(file1, file2):
+    rules = parse_rules(file1)
+    return sum(1 for line in file2 if matches(rules, '0', line))
+
+
+def part2(file1, file2):
+    rules = parse_rules(file1)
+    rules['8'] = [['42'], ['42', '8']]
+    rules['11'] = [['42', '31'], ['42', '11', '31']]
+
+    return sum(1 for line in file2 if matches(rules, '0', line))
+
+
+def main():
+    with open('input.txt') as f:
+        file_str1, file_str2 = f.read().split('\n\n')
+        file1 = file_str1.rstrip("\n").split('\n')
+        file2 = file_str2.rstrip("\n").split('\n')
+    print('Answer to puzzle 1: ')   
+    print(part1(list(file1), list(file2)))
+    print('')
+    print('Answer to puzzle 2: ')
+    print(part2(list(file1), list(file2)))
+
+
+if __name__ == '__main__':
+    main()
