f82c207b0b1419d5a9259cb2e29e817582ab87fc
João Victor <joaosantoos@aol.com>
2020-12-20 13:30:50 -0300

    day 18

--- Advent_of_code_2020/day18/solve18.py
+++ Advent_of_code_2020/day18/solve18.py
@@ -1,27 +1,53 @@
 #!/usr/bin/env python3
-import ast
 
+# Number wehre - is actually *
+class Num1:
+    def __init__(self, n):
+        self.n = n
+
+    def __add__(self, other):
+        return Num1(self.n + other.n)
+
+    def __sub__(self, other):
+        return Num1(self.n * other.n)
+
+# Number where + and * are swapped
+class Num2:
+    def __init__(self, n):
+        self.n = n
+
+    def __add__(self, other):
+        return Num2(self.n * other.n)
+
+    def __mul__(self, other):
+        return Num2(self.n + other.n)
+
+
+def eval_expr(expr, k="Num1"):
+    new_expr = ""
+
+    # Wrap all numbers in custom class
+    for c in expr:
+        if c.isdigit():
+            new_expr += f"{k}({c})"
+        else:
+            new_expr += c
+
+    if k == "Num1":
+        new_expr = new_expr.replace('*', '-')
+    else:
+        new_expr = new_expr.replace('+', '-').replace('*', '+').replace('-', '*')
+    return(eval(new_expr).n)
+
+
+solution_01 = 0
+solution_02 = 0
 with open('input.txt') as f:
-    ls = [line.strip() for line in f.readlines()]
-
-# https://www.geeksforgeeks.org/expression-evaluation/
-def evaluate(code):
-    root = ast.parse(code, mode='eval')
-    for node in ast.walk(root):
-        if type(node) is ast.BinOp:
-            node.op = ast.Add() if type(node.op) is ast.Div else ast.Mult()
-    return eval(compile(root, '<string>', 'eval'))
-
-
-# Replace + with / to decrease binding
+    for expr in f:
+        solution_01 += eval_expr(expr)
+        solution_02 += eval_expr(expr, "Num2")
 print('Answer to puzzle 1: ')
-print(sum(
-    evaluate(l.replace('+', '/')) for l in ls
-))
-
-# Replace * with + to increase binding
+print(solution_01)
 print('')
 print('Answer to puzzle 2: ')
-print(sum(
-    evaluate(l.replace('+', '/').replace('*', '+')) for l in ls
-))
+print(solution_02)
