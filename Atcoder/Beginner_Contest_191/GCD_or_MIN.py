9eb8e3b1dd0ccf7618668baf1616e2026d23a175
João Victor <joaosantoos@aol.com>
2021-02-25 10:51:51 -0300

    solution F

---
+++ Atcoder/Beginner_Contest_191/GCD_or_MIN.py
@@ -0,0 +1,31 @@
+import math
+
+N=int(input())
+A=list(map(int,input().split()))
+
+min_a=min(A)
+Yakusuu={}
+ 
+for i in range(N):
+  a=A[i]
+  i=1
+  yakusuu=[]
+  while i*i<=a:
+    if a%i==0:
+      if i<=min_a:
+        yakusuu.append(i)
+      if i!=a//i and a//i<=min_a: 
+        yakusuu.append(a//i)
+    i+=1
+  for t in yakusuu:
+    if t in Yakusuu:
+      Yakusuu[t]=math.gcd(Yakusuu[t],a)
+    else:
+      Yakusuu[t]=a
+      
+ans=0
+Ans=list(set(list(Yakusuu.values())))
+for x in Ans:
+  if x<=min_a:
+    ans+=1
+print(ans)
